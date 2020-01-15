317e447f0253a67e9954e1942c15c81203fcd59e
Jaehyeon <fourhf1@gmail.com>
2020-01-15 15:36:07 +0900

    Add routing signin and signup

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/signin', function(req, res, next) {
-   res.render('contents/signin');
+  res.render('contents/signin');
  });
+
+router.get('/signup', function(req, res, next) {
+  res.render('contents/signup');
+});
 
 module.exports = router;
